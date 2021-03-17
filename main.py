ce1afbc254068748b7a8468c9996929ead355bbb
Wael Alhashemi <alhashemiw@gmail.com>
2021-03-16 22:48:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,59 @@
 import discord
 import os
-from datetime import date
+from datetime import datetime, timedelta
+
+
+# Dub instance
+class Dub:
+  # Constructor
+  def __init__(self):
+    self.counter = 0
+  
+  # Function that increments the counter variable
+  def increment_dub(self):
+    self.counter += 1
+
+  def reset_counter(self):
+    self.counter = 0
 
 # client instance
 client = discord.Client()
 
+# our dub instance
+dub = Dub()
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client}')
-
+    
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+  if message.author == client.user:
+    return
 
-    # TODO:
-    #   ADD-ON: ADD A VICTORY SONG
-    if message.content.lower().startswith('!dub'):
-        await message.channel.send('Hello!')
+  if message.content.startswith('!dub'):
+    """
+      How to use: !dub
+      What it does: increments the counter attribute of the dub instance.
+    """
+    dub.increment_dub()
+    await message.channel.send(f'Dub counter: {dub.counter}')
 
-    # TODO:
-    #   IDEA: PLAY A SONG
-    if message.content.lower().startwith('TURN ME UP'):
-        await message.channel.send('TURN ME THE FUCK UPPPPPPPPPPPP')
+  elif message.content.startswith('!tmu'):
+    """
+      How to use: !tmu
+      What it does: Sends a hype message. #TMU
+    """
+    await message.channel.send('TURN ME UPPPPPPPPPPPP')
+  
+  elif message.content.startswith('!reset'):
+    """
+      How to use: !reset
+      What it does: Sets the counter attribute of the dub instance to 0
+    """
+    dub.reset_counter()
+    await message.channel.send(f'The dub counter has been reset! Current dub counter: {dub.counter}')
 
 
 client.run(os.getenv('TOKEN'))
