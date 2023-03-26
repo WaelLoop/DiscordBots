69da36952eecbc6e5dadf28cf009d81969a62994
WaelLoop <alhashemiw@gmail.com>
2023-03-26 16:38:02 -0400

    finished developing the commands

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import discord
 import os
 from datetime import datetime
-from Classes.Database import PostgreSQL
+#from Classes.database import PostgreSQL
+from Classes.dub import Dub
 
+
+# # database instance
+# db = PostgreSQL('hostname', 'db', 'username', 'password')
+# # Database table
+# DB_TABLE = 'Discord'
+# # Get wins from database command
+# GET_DUB_COMMAND = f'SELECT wins FROM {DB_TABLE} WHERE date_field = %s'
+# # Save wins to database command
+# SAVE_DUB_COMMAND = f'INSERT INTO {DB_TABLE} (date_field, wins) VALUES (date, numWins)'
 
 # client instance
 client = discord.Client()
-# database instance
-db = PostgreSQL('hostname', 'db', 'username', 'password')
-
-# Database table
-DB_TABLE = 'Discord'
-# Get wins from database command
-GET_DUB_COMMAND = f'SELECT wins FROM {DB_TABLE} WHERE date_field = %s'
-# Save wins to database command
-SAVE_DUB_COMMAND = f'INSERT INTO {DB_TABLE} (date_field, wins) VALUES (date, numWins)'
+# dub instance
+dub = Dub()
+# Current date
+currDate = datetime.now().strftime('%Y-%m-%d')
 
 # List of commands that our bot will know
 listOfCommands = [
@@ -23,9 +28,6 @@ listOfCommands = [
     '!getdub <YYYY-MM-DD>'
     '!help',
 ]
-# Current number of wins counter
-currWins = 0
-currDate = datetime.today().strftime("%Y-%m-%d")
 
 
 @client.event
@@ -49,23 +51,31 @@ async def on_message(message):
         
         # Match command with the list available
         if command == "dub":
-            currWins += 1
-            await message.channel.send(f'Dub counter: {currWins}')
+            dub.incrementDub()
+            await message.channel.send(f'Dub counter: {dub.getCounter()}')
 
         # Save the number of wins for the day to the database
         elif command == "gg":
             # Check if we have already gotten some wins
-            oldWins = db.read_data(GET_DUB_COMMAND.replace("%s", str(currDate)))
-            currWins += oldWins
+            # TODO: oldWins = db.read_data(GET_DUB_COMMAND.replace("%s", str(currDate)))
+            totalWins = dub.getWins(currDate)
+            totalWins += dub.getCounter()
             # Save the number of wins on the database
-            db.write_data(SAVE_DUB_COMMAND.replace("(date, numWins)", f'({currDate}, {currWins})'))
-            await message.channel.send(f'The number of wins the squad got today is {currWins}... GGs')
-            currWins = 0
+            # TODO: db.write_data(SAVE_DUB_COMMAND.replace("(date, numWins)", f'({currDate}, {currWins})'))
+            dub.storeWins(totalWins, currDate)
+            # Reset the counter
+            dub.resetCounter()
+            await message.channel.send(f'The number of wins the squad got today is {totalWins}... GGs')
 
         # Get the number of wins by providing a specific date in the format dd/mm/yy
         elif command == "getdub":
-            wins = db.read_data(GET_DUB_COMMAND.replace("%s", arguments))
-            await message.channel.send(f'The numbers of dubs the squad got on {arguments}: {wins}')
+            # TODO: wins = db.read_data(GET_DUB_COMMAND.replace("%s", arguments))
+            try:
+                datetime.strptime(arguments, '%Y-%m-%d')
+                wins = dub.getWins(arguments)
+                await message.channel.send(f'The numbers of dubs the squad got on {arguments}: {wins}')
+            except ValueError:
+                await message.channel.send(f'Invalid date format. The date must be in the format YYYY-MM-DD')
 
         # Help command to display available commands
         elif command == "help":
